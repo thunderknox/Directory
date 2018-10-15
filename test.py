62a71b0b7f19ac529cc3ae293ab865696a26c98c
Andy McGuire <thunderknox@gmail.com>
2018-10-15 09:13:09 +0100

    test file

---
+++ test.py
@@ -0,0 +1,26 @@
+from flask import Flask, abort
+app = Flask(__name__)
+
+@app.route("/") 
+def hello():
+	return "Hello Napier"
+
+
+@app.route("/static
+
+
+
+
+@app.route('/force404')
+def force404():
+	abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return	"Coudnt find the page SORRY!", 404
+
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', debug=True)
+
+
